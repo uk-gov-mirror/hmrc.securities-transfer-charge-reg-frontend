f67a1a9676e0bdadbb940d702da21d652dcb6c98
wp94 <43645818+wp94@users.noreply.github.com>
2026-06-23 15:01:44 +0100

    supply correlation id

--- app/uk/gov/hmrc/securitiestransferchargeregfrontend/clients/registration/RegistrationClient.scala
+++ app/uk/gov/hmrc/securitiestransferchargeregfrontend/clients/registration/RegistrationClient.scala
@@ -56,6 +56,7 @@ class RegistrationClientImpl @Inject()(
     val url = url"${config.viewSubscriptionBaseUrl}/$subscriptionId"
 
     http.get(url)
+      .setHeader(headers: _*)
       .execute[HttpResponse]
       .map { resp =>
         resp.status match {
